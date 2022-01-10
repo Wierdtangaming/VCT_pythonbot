a677ec1ff0d67504229cf184bcb621eb52ee2290
MaxOrtGit <maxrort@gmail.com>
2022-01-10 16:12:51 -0600

    token

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import discord
-
+import os
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print("Logged in as{0.user}".format(client))
+    print("Logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith("$match"):
+        await message.channel.send("Match")
+    
+    
+    
+client.run(os.environ.get("TOKEN"))
